0790180a0e5db735add2f5e8fe48c9b3e5d0dc62
Clement Laforet <sheepkiller@cotds.org>
2015-10-21 21:56:56 +0200

    improve inventory

--- inventory/ovh.py
+++ inventory/ovh.py
@@ -1,7 +1,24 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+#
 # Mostly a copy and paste from cobbler.py
 #
+# (c) 2012, Michael DeHaan <michael.dehaan@gmail.com> (as cobble.py author)
+# (c) 2015, Clement Laforet <clement.laforet@gmail.com> (for ovh support)
+#
+# Ansible is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# Ansible is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Ansible.  If not, see <http://www.gnu.org/licenses/>.
+
 
 import sys
 import argparse
@@ -15,12 +32,52 @@ try:
 except ImportError:
     import simplejson as json
 
+# Avoid to load ourself - Doesn't work with symlinks
+for path in [os.getcwd(), '', os.path.dirname(os.path.abspath(__file__))]:
+    try:
+        del sys.path[sys.path.index(path)]
+    except:
+        pass
+
 try:
     import ovh
 except ImportError:
-    print "failed=True msg='ovh required for this module'"
-#    sys.exit(1)
+    print "failed=True msg='ovh required for this software'"
+    sys.exit(1)
 
+try:
+    import ipaddress
+except ImportError:
+    print "failed=True msg='py2-ipaddress required for this software'"
+    sys.exit(1)
+
+def removeKey(d, key):
+    if key in d: del d[key]
+
+def cleanUpHost(d):
+    for k in d:
+        d[k.lower()] = d.pop(k)
+    if "ip" in d:
+        d['primary_ip'] = d.pop("ip")
+    if "reverse" in d:
+        d['reverse'] = d['reverse'].rstrip('.')
+
+removeArgsVps = [
+    "monitoringIpBlocks",
+    "keymap",
+    "model"
+]
+
+removeArgsServer = [
+    "commercialRange",
+    "professionalUse",
+    "rescueMail",
+    "rootDevice",
+    "serverId",
+    "state",
+    "supportLevel",
+    "bootId"
+]
 class OvhInventory(object):
 
     def __init__(self):
@@ -72,12 +129,15 @@ class OvhInventory(object):
 
     def read_settings(self):
         """ Reads the settings from the ovh.ini file """
-
+        pattern = re.compile(r'\s+')
         config = ConfigParser.SafeConfigParser()
         config.read(os.path.dirname(os.path.realpath(__file__)) + '/ovh.ini')
         self.regions = []
-        configRegions = config.get('ovh', 'regions')
+        configRegions = re.sub(pattern, '', config.get('ovh', 'regions'))
         self.regions = configRegions.split(",")
+        configGroupby = re.sub(pattern, '',config.get('ovh', 'group_by'))
+        self.Groupby = configGroupby.split(",")
+        self.configHostname = config.get('ovh', 'hostname')
 
 
         # Cache related
@@ -96,47 +156,78 @@ class OvhInventory(object):
                             help='Force refresh of cache by making API requests to ovh (default: False - use cache files)')
         self.args = parser.parse_args()
 
-    def get_vps(self):
+    def add_to_cache(self, d, type,region):
+        d["region"] = region
+        d["type"] = type
+        cleanUpHost(d)
+        host = None
+        if self.configHostname == "primary_ip":
+            host = d["primary_ip"]
+        if self.configHostname == "servicename":
+            host = d["name"]
+        if self.configHostname == "customname":
+            fallback = d["primary_ip"] if "reverse" not in d else d["reverse"]
+            host = fallback if "displayname" not in d else d["displayname"]
+
+        self.cache[host] = d
+        for gb in self.Groupby:
+            if gb in d:
+                self.push(self.inventory, d[gb] , host)
+
+    def get_vps(self, region):
         data = self.conn.get('/vps')
         for host in data:
             vps = dict()
             #self.cache[host] = host
-            info = self.conn.get('/vps/' + host)
-            vps['ovh_name']= host
-            vps['zone'] = info['zone']
-            ips = self.conn.get('/vps/' + host + '/ips')
+            vps = self.conn.get('/vps/{}'.format(host))
+            for k in removeArgsVps:
+                removeKey(vps, k)
+            ips = self.conn.get('/vps/{}/ips'.format(host))
+            list_ips=dict()
             for ip in ips:
-                ip_info = self.conn.get('/vps/' + host + '/ips/' + ip)
-                key = "ip" + str.join("-", [ip_info["version"]])
-            self.cache[host] = vps
-            self.push(self.inventory, "vps", host)
+                ip_info = self.conn.get('/vps/{0}/ips/{1}'.format(host,ip))
+                v = ip_info["version"]
+                if ip_info["type"] == "primary" and v == "v4":
+                    vps["ip"] = ip
+                if v not in list_ips:
+                    list_ips[v] = []
+                list_ips[v].append(ip)
 
-    def get_dedicated(self):
+            vps["ips"]=list_ips
+            self.add_to_cache(vps, "vps", region)
+
+
+    def get_dedicated(self, region):
         data = self.conn.get('/dedicated/server')
         for host in data:
             c = dict()
             server = dict()
             #self.cache[host] = host
-            info = self.conn.get('/dedicated/server/' + host)
-            server['ovh_name']= host
-            server['datacenter'] = info['datacenter']
-            c["ovh_name"] = host
-            server["ip"] = c["main_ip"] = info['ip']
-            c["reverse"] = info["reverse"] if info["reverse"] else info['ip']
-            server["reverse"] = info["reverse"]
-            ips = self.conn.get('/dedicated/server/' + host + '/ips')
+            server = self.conn.get('/dedicated/server/{}'.format(host))
+            c["reverse"] = server["reverse"] if server["reverse"] else server['ip']
+            for k in removeArgsServer:
+                removeKey(server, k)
+            ips = self.conn.get('/dedicated/server/{}/ips'.format(host))
+            list_ips = dict()
+            for _ip in ips:
+                ip = _ip.split('/')[0]
+                ip_info = dict()
+                v = 'v' + str(ipaddress.ip_address(ip).version).lower()
+                if v not in list_ips:
+                    list_ips[v] = []
+                list_ips[v].append(ip)
 
-            self.cache[c["reverse"]] = server
-            self.push(self.inventory, "servers", c["reverse"])
+            server["ips"]=list_ips
+            self.add_to_cache(server, "server", region)
 
     def update_cache(self):
         """ Make calls to ovh and save the output in a cache """
         self.groups = dict()
         self.hosts = dict()
         for region in self.regions:
-            self.conn = ovh.Client()
-            self.get_vps()
-            self.get_dedicated()
+            self.conn = ovh.Client(endpoint=region)
+            self.get_vps(region)
+            self.get_dedicated(region)
 
         self.write_to_cache(self.cache, self.cache_path_cache)
         self.write_to_cache(self.inventory, self.cache_path_inventory)
